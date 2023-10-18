d99dd658bd3dd34ad3b156058f2b01b812ec2420
Julian <j.brecht@live.de>
2023-10-18 07:13:31 +0200

    Initialized Project

---
+++ src/main/java/ch/csbe/productstore/ProductStoreApplication.java
@@ -0,0 +1,13 @@
+package ch.csbe.productstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/csbe/productstore/ProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.csbe.productstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
