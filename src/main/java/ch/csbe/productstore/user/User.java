9056553c2d4299c375cc533b1298e000ecbff33c
Julian <j.brecht@live.de>
2023-10-20 15:19:03 +0200

    Added getter + setter for User

--- src/main/java/ch/csbe/productstore/user/User.java
+++ src/main/java/ch/csbe/productstore/user/User.java
@@ -24,4 +24,28 @@ public class User {
     public Long getId() {
         return id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
 }
