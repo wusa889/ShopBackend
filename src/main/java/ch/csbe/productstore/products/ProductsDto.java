f48e88a1d0fc3a1cddad942f3235df867c4d56f6
wusa889 <144148191+wusa889@users.noreply.github.com>
2023-10-21 10:36:20 +0200

    Defined Products DTO

--- src/main/java/ch/csbe/productstore/products/ProductsDto.java
+++ src/main/java/ch/csbe/productstore/products/ProductsDto.java
@@ -1,4 +1,64 @@
 package ch.csbe.productstore.products;
 
 public class ProductsDto {
+
+    private long id;
+
+    private String name;
+
+    private String image;
+
+    private String description;
+
+    private float price;
+
+    private int stock;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
 }
