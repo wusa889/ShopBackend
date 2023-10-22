b711d9761374ef2f6d86ced0bb5b69478f98a677
wusa889 <144148191+wusa889@users.noreply.github.com>
2023-10-22 13:12:16 +0200

    Added AuthService and Controller

--- src/main/java/ch/csbe/productstore/auth/AuthService.java
+++ src/main/java/ch/csbe/productstore/auth/AuthService.java
@@ -1,4 +1,37 @@
 package ch.csbe.productstore.auth;
 
+import ch.csbe.productstore.user.User;
+import ch.csbe.productstore.user.UserDto;
+import ch.csbe.productstore.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    public String getJwt(UserDto userDto) {
+        User user = userRepository.findByUserName(userDto.getUsername())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if (!bCryptPasswordEncoder.matches(userDto.getPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        return jwtService.createJwt(user.getUsername());
+    }
+
+
+
+
 }
