2d57709d0ebc25bb9f891000aad45fef603a2e6e
wusa889 <144148191+wusa889@users.noreply.github.com>
2023-10-22 14:33:20 +0200

    Changed AuthConfig and added TODO

--- src/main/java/ch/csbe/productstore/auth/AuthConfig.java
+++ src/main/java/ch/csbe/productstore/auth/AuthConfig.java
@@ -1,11 +1,35 @@
 package ch.csbe.productstore.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 
+@Configuration
 public class AuthConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                    //TODO Authorisation auf die einzelnen Endpoints setzen wie in LB verlangt.
+                    authorizationManagerRequestMatcherRegistry.requestMatchers("/user").permitAll();
+                    authorizationManagerRequestMatcherRegistry.requestMatchers("/user/login").permitAll();
+                })
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
 
 
 }
